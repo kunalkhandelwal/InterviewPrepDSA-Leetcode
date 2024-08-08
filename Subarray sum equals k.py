7b17144f587fafc0354cf22075950d12adaedf73
Kunal Khandelwal <54462993+kunalkhandelwal@users.noreply.github.com>
2024-08-08 18:52:01 -0400

    Update Subarray sum equals k.py

--- Subarray sum equals k.py
+++ Subarray sum equals k.py
@@ -1,7 +1,23 @@
 #https://leetcode.com/problems/subarray-sum-equals-k/description/
 
-#OPTIMAL SOLUTION
+#OPTIMAL SOLUTION - Beats - 69, 53
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        ans = 0
+        prefix_sum = 0
+        d = {0:1}
 
+        for num in nums:
+
+            prefix_sum = prefix_sum + num
+
+            if (prefix_sum - k) in d:
+                ans = ans + d[prefix_sum - k]
+            if (prefix_sum) not in d:
+                d[prefix_sum] = 1
+            else:
+                d[prefix_sum] = d[prefix_sum] + 1
+        return ans
 
 
 #Sol 1: TIME LIMIT EXCEEDED
