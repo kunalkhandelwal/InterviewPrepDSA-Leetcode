be1297da6206b60d093f6504bc51ec70af108f5a
Kunal Khandelwal <54462993+kunalkhandelwal@users.noreply.github.com>
2024-09-02 16:58:13 -0700

    Update 06 Find the Number of Good Pairs 1.py

--- 06 Find the Number of Good Pairs 1.py
+++ 06 Find the Number of Good Pairs 1.py
@@ -1,7 +1,17 @@
 #https://leetcode.com/problems/find-the-number-of-good-pairs-i/submissions/1375848661/
 
-Optimal Solution:
 
+
+#Optimal Solution: Beats 68, 68
+class Solution:
+    def numberOfPairs(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        ans = 0
+        
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                if nums1[i]%(nums2[j]*k)==0:
+                    ans+=1
+        return ans
 
 #Sol 0: Beats - 29, 18
 class Solution:
