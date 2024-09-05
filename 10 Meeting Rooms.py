c8bdada115f6e6d805d9094c4dc051bf544d643a
Kunal Khandelwal <54462993+kunalkhandelwal@users.noreply.github.com>
2024-09-05 15:20:02 -0700

    Create 10 Meeting Rooms.py

---
+++ 10 Meeting Rooms.py
@@ -0,0 +1,9 @@
+#Sol 1:
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        intervals = sorted(intervals)
+
+        for i in range(len(intervals)-1):
+            if intervals[i][1] > intervals[i+1][0]:
+                return False
+        return True
