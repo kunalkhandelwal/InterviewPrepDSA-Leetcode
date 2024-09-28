2ffa13146098f9b46de72078ea556fab766de14e
Kunal Khandelwal <54462993+kunalkhandelwal@users.noreply.github.com>
2024-09-28 00:25:27 -0700

    Create 51 Crawler Log Folder.py

---
+++ 51 Crawler Log Folder.py
@@ -0,0 +1,23 @@
+#Sol 0: Beats - 75, 6
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        val = 0
+
+        for i in range(len(logs)):
+            if logs[i] == "./":
+                continue
+            elif val==0 and (logs[i] == "../" or logs[i] == "./"):
+                continue                
+            elif logs[i] == "../" and val!=0:
+                val -= 1
+            else:
+                val+= 1
+            print(val)
+
+        if val<0:
+            return 0
+        else:
+            return val
+
+      #Sol 1: With Stack
+      
