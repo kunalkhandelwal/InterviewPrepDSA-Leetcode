067b1671ff05ee05f52dd990d7798a6fb1535c2b
Kunal Khandelwal <54462993+kunalkhandelwal@users.noreply.github.com>
2024-10-02 00:03:24 -0700

    Create 64 Find Target Indices after Sorting.py

---
+++ 64 Find Target Indices after Sorting.py
@@ -0,0 +1,12 @@
+#Sol 0: Beats - 99, 37
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+
+        nums.sort()
+        l = []
+        
+        for i in range(len(nums)):
+            if nums[i] ==  target:
+                l.append(i)
+
+        return l
